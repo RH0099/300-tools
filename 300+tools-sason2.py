d5c3d21966577a5f64e9a3769fd5d7a9d21735fa
R,H <162550688+RH0099@users.noreply.github.com>
2024-06-02 20:06:58 +0600

    Update 300+tools-sason2.py

--- 300+tools-sason2.py
+++ 300+tools-sason2.py
@@ -504,3 +504,411 @@ echo -e "============================" | lolcat
 echo -e $b "20. Install Termux-Banner${enda}";
 echo -e "============================" | lolcat
 echo -e $b "21. Install udfhack${enda}";
+echo -e "============================" | lolcat
+echo -e $b "22. Install Mirai-Source-Code${enda}";
+echo -e "============================" | lolcat
+echo -e $b "23. Install Hale${enda}";
+echo -e "============================" | lolcat
+echo -e $b "24. Install Mirai-IoT-BotNet${enda}";
+echo -e "============================" | lolcat
+echo -e $b "25. Install Ufonet${enda}";
+echo -e "============================" | lolcat
+echo -e $b "26. Install bom${enda}";
+echo -e "============================" | lolcat
+echo -e $b "27. Install tool${enda}";
+echo -e "============================" | lolcat
+echo -e $b "28. Install BotNet${enda}";
+echo -e "============================" | lolcat
+echo -e $b "29. Install Malwares${enda}";
+echo -e "============================" | lolcat
+echo -e $b "30. Install zeus-bot${enda}";
+echo -e "============================" | lolcat
+echo -e $b "31. Install AutoSploit${enda}";
+echo -e "============================" | lolcat
+echo -e $b "32. Install IFC${enda}";
+echo -e "============================" | lolcat
+echo -e $b "33. Install SQLMAP${enda}";
+echo -e "============================" | lolcat
+echo -e $b "34. Install Spyder${enda}";
+echo -e "============================" | lolcat
+echo -e $b "35. Install Social-Engineer-ToolKit${enda}";
+echo -e "============================" | lolcat
+echo -e $b "36. Install Kawai-Botnet${enda}";
+echo -e "============================" | lolcat
+echo -e $b "37. Install DarkSploit${enda}";
+echo -e "============================" | lolcat
+echo -e $b "38. Install SH33LL${enda}";
+echo -e "============================" | lolcat
+echo -e $b "39. Install Evil-Create-Framework${enda}";
+echo -e "============================" | lolcat
+echo -e $b "40. Install Gabutz${enda}";
+echo -e "============================" | lolcat
+echo -e $b "41. Install RootNet${enda}";
+echo -e "============================"| lolcat
+echo -e $b "42. Install BadMod${enda}";
+echo -e "============================" | lolcat
+echo -e $b "43. Install BoomHash${enda}";
+echo -e "============================" | lolcat
+echo -e $b "44. Install Plutus${enda}";
+echo -e "============================" | lolcat
+echo -e $b "45. Install ContexPloit${enda}";
+echo -e "============================" | lolcat
+echo -e $b "46. Install Th3inspector${enda}";
+echo -e "============================" | lolcat
+echo -e $b "47. Install Findip${enda}";
+echo -e "============================" | lolcat
+echo -e $b "48. Install V3nom-Scanner${enda}";
+echo -e "============================" | lolcat
+echo -e $b "49. Install Bom Sms 3${enda}";
+echo -e "============================" | lolcat
+echo -e $b "50. Install WebKiller${enda}";
+echo -e "============================" | lolcat
+echo -e $b "51. Install Bitcoin-Hacking-Tools${enda}";
+echo -e "============================" | lolcat
+echo -e $b "52. Install Firefox-Plugin-Popup-Logout${enda}";
+echo -e "============================" | lolcat
+echo -e $b "53. Install Bitcoin-All-Key-Generator${enda}";
+echo -e "============================" | lolcat
+echo -e $b "54. Install My-First-Bitcoin-Miner${enda}";
+echo -e "============================" | lolcat
+echo -e $b "55. Install Parity-Config-Generator${enda}";
+echo -e "============================" | lolcat
+echo -e $b "56. Install Distributed-Bitcoin-Generator${enda}";
+echo -e "============================" | lolcat
+echo -e $b "57. Install Mesos-Bitcoin-Miner${enda}";
+echo -e "============================" | lolcat
+echo -e $b "58. Install Git_Psibot_Hacking${enda}";
+echo -e "============================" | lolcat
+echo -e $b "59. Install KatanaFramework${enda}";
+echo -e "============================" | lolcat
+echo -e $b "60. Install STP${enda}";
+echo -e "============================" | lolcat
+echo -e $b "61. Install Termux-Ubuntu${enda}";
+echo -e "============================" | lolcat
+echo -e $b "62. Install Nethuner-In-Termux${enda}";
+echo -e "============================" | lolcat
+echo -e $b "63. Install viSQL${enda}";
+echo -e "============================" | lolcat
+echo -e $b "64. Install Termux-Archlinux${enda}";
+echo -e "============================" | lolcat
+echo -e $b "65. Install Santet-Online${enda}";
+echo -e "============================" | lolcat
+echo -e $b "66. Install GadoGado${enda}";
+echo -e "============================" | lolcat
+echo -e $b "67. Install CnkSpam${enda}";
+echo -e "============================" | lolcat
+echo -e $b "68. Install AutoReportFB${enda}";
+echo -e "============================" | lolcat
+echo -e $b "69. Install Google-Dork${enda}";
+echo -e "============================" | lolcat
+echo -e $b "70. Install FHX-Hash-Killer${enda}";
+echo -e "============================" | lolcat
+echo -e $b "71. Install Hash-Buster${enda}";
+echo -e "============================" | lolcat
+echo -e $b "72. Install Metasploit${enda}";
+echo -e "============================" | lolcat
+echo -e $b "73. Install Striker${enda}";
+echo -e "============================" | lolcat
+echo -e $b "74. Install AutoScriptKiddieTool${enda}";
+echo -e "============================" | lolcat
+echo -e $b "75. Install Weeman${enda}";
+echo -e "============================" | lolcat
+echo -e $b "76. Install SCANNER-INURLBR${enda}";
+echo -e "============================" | lolcat
+echo -e $b "77. Install Script-Deface-Creator${enda}";
+echo -e "============================" | lolcat
+echo -e $b "78. Install ktpKkGenerate${enda}";
+echo -e "============================" | lolcat
+echo -e $b "79. Install ReconDog${enda}";
+echo -e "============================" | lolcat
+echo -e $b "80. Install HakkuFramework${enda}";
+echo -e "============================" | lolcat
+echo -e $b "81. Install HunnerFramework${enda}";
+echo -e "============================" | lolcat
+echo -e $b "82. Install Hammer${enda}";
+echo -e "============================" | lolcat
+echo -e $b "83. Install Torshammer${enda}";
+echo -e "============================" | lolcat
+echo -e $b "84. Install Katoolin${enda}";
+echo -e "============================" | lolcat
+echo -e $b "85. Install MPSYT${enda}";
+echo -e "============================" | lolcat
+echo -e $b "86. Install A-Rat${enda}";
+echo -e "============================" | lolcat
+echo -e $b "87. Install Cupp${enda}";
+echo -e "============================" | lolcat
+echo -e $b "88. Install Webpwn3r${enda}";
+echo -e "============================" | lolcat
+echo -e $b "89. Install IPGeolocation${enda}";
+echo -e "============================" | lolcat
+echo -e $b "90. Install XL${enda}";
+echo -e "============================" | lolcat
+echo -e $b "91. Install BotFbBangDjon${enda}";
+echo -e "============================" | lolcat
+echo -e $b "92. Install 4wsectools${enda}";
+echo -e "============================" | lolcat
+echo -e $b "93. Install Admin_Penal${enda}";
+echo -e "============================" | lolcat
+echo -e $b "94. Install RouterSploit${enda}";
+echo -e "============================" | lolcat
+echo -e $b "95. Install RusSpam${enda}";
+echo -e "============================" | lolcat
+echo -e $b "96. Install Jexboss${enda}";
+echo -e "============================" | lolcat
+echo -e $b "97. Install WifiPhisher${enda}";
+echo -e "============================" | lolcat
+echo -e $b "98. Install WebSploit${enda}";
+echo -e "============================" | lolcat
+echo -e $b "99. Install Wifi-Hacker${enda}";
+echo -e "============================" | lolcat
+echo -e $b "100. Install Mr.Rv1${enda}";
+echo -e "============================" | lolcat
+echo -e $b "101. Install Mr.Rv.2${enda}";
+echo -e "============================" | lolcat
+echo -e $b "102. Install Stegosploit${enda}";
+echo -e "============================" | lolcat
+echo -e $b "103. Install Blazy${enda}";
+echo -e "============================" | lolcat
+echo -e $b "104. Install anonymous${enda}";
+echo -e "============================" | lolcat
+echo -e $b "105. Install Bingoo${enda}";
+echo -e "============================" | lolcat
+echo -e $b "106. Install Tool-X${enda}";
+echo -e "============================" | lolcat
+echo -e $b "107. Install kickthemout${enda}";
+echo -e "============================" | lolcat
+echo -e $b "108. Install T.DYf[300Tools]${enda}";
+echo -e "============================" | lolcat
+echo -e $b "109. Install HPAS-1369${enda}";
+echo -e "============================" | lolcat
+echo -e $b "110. Install pemulungBTC${enda}";
+echo -e "============================" | lolcat
+echo -e $b "111. Install TouchUrl${enda}";
+echo -e "============================" | lolcat
+echo -e $b "112. Install IP-TRACK${enda}";
+echo -e "============================" | lolcat
+echo -e $b "113. Install Kuyang-Tool${enda}";
+echo -e "============================" | lolcat
+echo -e $b "114. Install SpazSMS${enda}";
+echo -e "============================" | lolcat
+echo -e $b "115. Install SiteBroker${enda}";
+echo -e "============================" | lolcat
+echo -e $b "116. Install Email-Bomber${enda}";
+echo -e "============================" | lolcat
+echo -e $b "117. Install Ip-Gathering${enda}";
+echo -e "============================" | lolcat
+echo -e $b "118. Install Scorpion${enda}";
+echo -e "============================" | lolcat
+echo -e $b "119. Install New-Spammer${enda}";
+echo -e "============================" | lolcat
+echo -e $b "120. Install Spam${enda}";
+echo -e "============================" | lolcat
+echo -e $b "121. Install QJDID${enda}";
+echo -e "============================" | lolcat
+echo -e $b "122. Install QFloodSms${enda}";
+echo -e "============================" | lolcat
+echo -e $b "123. Install Login-Termux${enda}";
+echo -e "============================" | lolcat
+echo -e $b "124. Install Linux${enda}";
+echo -e "============================" | lolcat
+echo -e $b "125. Install Komodo${enda}";
+echo -e "============================" | lolcat
+echo -e $b "126. Install HN-Installer${enda}";
+echo -e "============================" | lolcat
+echo -e $b "127. Install Deface-Create${enda}";
+echo -e "============================" | lolcat
+echo -e $b "128. Install Good_Terminal${enda}";
+echo -e "============================" | lolcat
+echo -e $b "129. Install Saddam${enda}";
+echo -e "============================" | lolcat
+echo -e $b "130. Install SqliteBrowser${enda}";
+echo -e "============================" | lolcat
+echo -e $b "131. Install PoC-Exploit${enda}";
+echo -e "============================" | lolcat
+echo -e $b "132. Install VTools${enda}";
+echo -e "============================" | lolcat
+echo -e $b "133. Install Termux-Loginv2fx${enda}";
+echo -e "============================" | lolcat
+echo -e $b "134. Install Anti-DDOS${enda}";
+echo -e "============================" | lolcat
+echo -e $b "135. Install NScan${enda}";
+echo -e "============================" | lolcat
+echo -e $b "136. Install Hostcheker${enda}";
+echo -e "============================" | lolcat
+echo -e $b "137. Install WebKit${enda}";
+echo -e "============================" | lolcat
+echo -e $b "138. Install AOCDEFACE${enda}";
+echo -e "============================" | lolcat
+echo -e $b "139. Install Face-Hack${enda}";
+echo -e "============================" | lolcat
+echo -e $b "140. Install Bash-Ransomware${enda}";
+echo -e "============================" | lolcat
+echo -e $b "141. Install Cli-Browser${enda}";
+echo -e "============================" | lolcat
+echo -e $b "142. Install Spam-Mantan${enda}";
+echo -e "============================" | lolcat
+echo -e $b "143. Install ngrok-stable-linux-arm${enda}";
+echo -e "============================" | lolcat
+echo -e $b "144. Install Hulk${enda}";
+echo -e "============================" | lolcat
+echo -e $b "145. Install Termux-Lazsqlmap${enda}";
+echo -e "============================" | lolcat
+echo -e $b "146. Install Shellnoob${enda}";
+echo -e "============================" | lolcat
+echo -e $b "147. Install ATSCAN${enda}";
+echo -e "============================" | lolcat
+echo -e $b "148. Install Commix${enda}";
+echo -e "============================" | lolcat
+echo -e $b "149. Install Wpscan${enda}";
+echo -e "============================" | lolcat
+echo -e $b "150. Install wpbf${enda}";
+echo -e "============================" | lolcat
+echo -e $b "151. Install GreenReaper${enda}";
+echo -e "============================" | lolcat
+echo -e $b "152. Install Devploit${enda}";
+echo -e "============================" | lolcat
+echo -e $b "153. Install Ipmux${enda}";
+echo -e "============================" | lolcat
+echo -e $b "154. Install Genscript${enda}";
+echo -e "============================" | lolcat
+echo -e $b "155. Install Airgeddon${enda}";
+echo -e "============================" | lolcat
+echo -e $b "156. Install AVARSpam${enda}";
+echo -e "============================" | lolcat
+echo -e $b "157. Install ANRSpam${enda}";
+echo -e "============================" | lolcat
+echo -e $b "158. Install Termux-ohmyzsh${enda}";
+echo -e "============================" | lolcat
+echo -e $b "159. Install CredSniper${enda}";
+echo -e "============================" | lolcat
+echo -e $b "160. Install Fluxion${enda}";
+echo -e "============================" | lolcat
+echo -e $b "161. Install pixiewps${enda}";
+echo -e "============================" | lolcat
+echo -e $b "162. Install wifite${enda}";
+echo -e "============================" | lolcat
+echo -e $b "163. Install Zones${enda}";
+echo -e "============================" | lolcat
+echo -e $b "164. Install sqlokmed${enda}";
+echo -e "============================" | lolcat
+echo -e $b "165. Install Sir${enda}";
+echo -e "============================" | lolcat
+echo -e $b "166. Install Easymap${enda}";
+echo -e "============================" | lolcat
+echo -e $b "167. Install Sqliv${enda}";
+echo -e "============================" | lolcat
+echo -e $b "168. Install AndroidPinCrack${enda}";
+echo -e "============================" | lolcat
+echo -e $b "169. Install NetKiller${enda}";
+echo -e "============================" | lolcat
+echo -e $b "170. Install IPscan${enda}";
+echo -e "============================" | lolcat
+echo -e $b "171. Install XPL-SEARCH${enda}";
+echo -e "============================" | lolcat
+echo -e $b "172. Install Bolang${enda}";
+echo -e "============================" | lolcat
+echo -e $b "173. Install Termux-Go${enda}";
+echo -e "============================" | lolcat
+echo -e $b "174. Install Toolss${enda}";
+echo -e "============================" | lolcat
+echo -e $b "175. Install AngryFuzzer${enda}";
+echo -e "============================" | lolcat
+echo -e $b "176. Install Hacktronian${enda}";
+echo -e "============================" | lolcat
+echo -e $b "177. Install EvilURL${enda}";
+echo -e "============================" | lolcat
+echo -e $b "178. Install CredMap${enda}";
+echo -e "============================" | lolcat
+echo -e $b "179. Install Pybelt${enda}";
+echo -e "============================" | lolcat
+echo -e $b "180. Install PyBozoCrack${enda}";
+echo -e "============================" | lolcat
+echo -e $b "181. Install Hashzer${enda}";
+echo -e "============================" | lolcat
+echo -e $b "182. Install Poet${enda}";
+echo -e "============================" | lolcat
+echo -e $b "183. Install MamangKey${enda}";
+echo -e "============================" | lolcat
+echo -e $b "184. Install Termux-Sudo${enda}";
+echo -e "============================" | lolcat
+echo -e $b "185. Install Nikto${enda}";
+echo -e "============================" | lolcat
+echo -e $b "186. Install Tuyulbtn${enda}";
+echo -e "============================" | lolcat
+echo -e $b "187. Install Fsociety${enda}";
+echo -e "============================" | lolcat
+echo -e $b "188. Install 1337Hash${enda}";
+echo -e "============================" | lolcat
+echo -e $b "189. Install CyberScan${enda}";
+echo -e "============================" | lolcat
+echo -e $b "190. Install Bitcoin-Wallet${enda}";
+echo -e "============================" | lolcat
+echo -e $b "191. Install CamStream-V3${enda}";
+echo -e "============================" | lolcat
+echo -e $b "192. Install XEIT_CYBER${enda}";
+echo -e "============================" | lolcat
+echo -e $b "193. Install CsrfPocMaker${enda}";
+echo -e "============================" | lolcat
+echo -e $b "194. Install PHP-BackConnector${enda}";
+echo -e "============================" | lolcat
+echo -e $b "195. Install StabilizerBot${enda}";
+echo -e "============================" | lolcat
+echo -e $b "196. Install Facebook-Video-Downloader${enda}";
+echo -e "============================" | lolcat
+echo -e $b "197. Install Rembot${enda}";
+echo -e "============================" | lolcat
+echo -e $b "198. Install Entropy${enda}";
+echo -e "============================'' | lolcat
+echo -e $b "199. Install nmap${enda}";
+echo -e "============================" | lolcat
+echo -e $b "160. Install Fluxion${enda}";
+echo -e "============================" | lolcat
+echo -e $b "161. Install pixiewps${enda}";
+echo -e "============================" | lolcat
+echo -e $b "162. Install wifite${enda}";
+echo -e "============================" | lolcat
+echo -e $b "163. Install Zones${enda}";
+echo -e "============================" | lolcat
+echo -e $b "164. Install sqlokmed${enda}";
+echo -e "============================" | lolcat
+echo -e $b "165. Install Sir${enda}";
+echo -e "============================" | lolcat
+echo -e $b "166. Install Easymap${enda}";
+echo -e "============================" | lolcat
+echo -e $b "167. Install Sqliv${enda}";
+echo -e "============================" | lolcat
+echo -e $b "168. Install AndroidPinCrack${enda}";
+echo -e "============================" | lolcat
+echo -e $b "169. Install NetKiller${enda}";
+echo -e "============================" | lolcat
+echo -e $b "170. Install IPscan${enda}";
+echo -e "============================" | lolcat
+echo -e $b "171. Install XPL-SEARCH${enda}";
+echo -e "============================" | lolcat
+echo -e $b "172. Install Bolang${enda}";
+echo -e "============================" | lolcat
+echo -e $b "173. Install Termux-Go${enda}";
+echo -e "============================" | lolcat
+echo -e $b "174. Install Toolss${enda}";
+echo -e "============================" | lolcat
+echo -e $b "175. Install AngryFuzzer${enda}";
+echo -e "============================" | lolcat
+echo -e $b "176. Install Hacktronian${enda}";
+echo -e "============================" | lolcat
+echo -e $b "177. Install EvilURL${enda}";
+echo -e "============================" | lolcat
+echo -e $b "178. Install CredMap${enda}";
+echo -e "============================" | lolcat
+echo -e $b "179. Install Pybelt${enda}";
+echo -e "============================" | lolcat
+echo -e $b "180. Install PyBozoCrack${enda}";
+echo -e "============================" | lolcat
+echo -e $b "181. Install Hashzer${enda}";
+echo -e "============================" | lolcat
+echo -e $b "182. Install Poet${enda}";
+echo -e "============================" | lolcat
+echo -e $b "183. Install MamangKey${enda}";
+echo -e "============================" | lolcat
+echo -e $b "184. Install Termux-Sudo${enda}";
+echo -e "============================" | lolcat
+echo -e $b "185. Install Nikto${enda}";
